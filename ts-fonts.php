280c2b4cab666a4d5d00155fabbc558f0512ad22
hina <hina@hinaloe.net>
2016-03-31 04:11:46 +0900

    code style

--- ts-fonts.php
+++ ts-fonts.php
@@ -9,27 +9,31 @@ Text Domain: nippombashi2016
 Domain Path: /languages
  */
 
-if( ! defined ( 'ABSPATH' ) ) {
-  exit(1);
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 1 );
 }
 
-function tsf_loadcss () {
-if ( ! function_exists( 'twentysixteen_setup' ) ) {
-  return;
+function tsf_loadcss() {
+	if ( ! function_exists( 'twentysixteen_setup' ) ) {
+		return;
+	}
+	$extension = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? 'css' : 'min.css';
+	wp_enqueue_style( 'twentysixteen-japanese-font', plugin_dir_url( __FILE__ ) . 'css/fonts.' . $extension, array( 'twentysixteen-style' ) );
 }
-  $extension = defined ( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? 'css' : 'min.css';
-  wp_enqueue_style ( 'twentysixteen-japanese-font', plugin_dir_url (__FILE__ ) . 'css/fonts.' . $extension ,array( 'twentysixteen-style' ) );
-}
-add_action ( 'wp_enqueue_scripts', 'tsf_loadcss', 11 );
+
+add_action( 'wp_enqueue_scripts', 'tsf_loadcss', 11 );
 
 function tsf_editorcss( $mce_css ) {
-  if ( ! function_exists( 'twentysixteen_setup' ) ) {
-    return;
-  }
-  $extension = defined ( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? 'css' : 'min.css';
-	if ( ! empty( $mce_css ) )
+	if ( ! function_exists( 'twentysixteen_setup' ) ) {
+		return;
+	}
+	$extension = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? 'css' : 'min.css';
+	if ( ! empty( $mce_css ) ) {
 		$mce_css .= ',';
-	$mce_css .= plugins_url ( 'css/fonts.' . $extension , __FILE__ );
+	}
+	$mce_css .= plugins_url( 'css/fonts.' . $extension, __FILE__ );
+
 	return $mce_css;
 }
+
 add_filter( 'mce_css', 'tsf_editorcss' );
